5d4282e7df71da0dfdbfad579742d706669f9b51
DeveloperRooney <han_0427@naver.com>
2022-03-19 23:47:54 +0900

    querydsl 세팅 및 테스트

--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Item;
+import com.study.querydsl.entity.QItem;
+import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    private EntityManager em;
 
     @Test
     void contextLoads() {
+        Item item = new Item();
+        em.persist(item);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QItem qItem = new QItem("item");
+
+        Item result = query.selectFrom(qItem)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(item);
     }
 
 }
