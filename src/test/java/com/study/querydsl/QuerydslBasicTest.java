160c106d9fb3f589eb3e05d746d3306a2a150ef0
DeveloperRooney <han_0427@naver.com>
2022-03-20 19:01:36 +0900

    JPQL과 Querydsl 비교

---
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,63 @@
+package com.study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Member;
+import com.study.querydsl.entity.QMember;
+import com.study.querydsl.entity.Team;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    private EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member memberA = new Member("memberA", 10, teamA);
+        Member memberB = new Member("memberB", 20, teamA);
+        Member memberC = new Member("memberC", 30, teamB);
+        Member memberD = new Member("memberD", 40, teamB);
+        em.persist(memberA);
+        em.persist(memberB);
+        em.persist(memberC);
+        em.persist(memberD);
+    }
+
+    @Test
+    public void startJPQL() {
+        // memberA 조회
+        Member findMember = em.createQuery("select m from Member m where m.memberName = :memberName", Member.class)
+                .setParameter("memberName", "memberA")
+                .getSingleResult();
+
+        Assertions.assertThat(findMember.getMemberName()).isEqualTo("memberA");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.memberName.eq("memberA"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getMemberName()).isEqualTo("memberA");
+    }
+}
